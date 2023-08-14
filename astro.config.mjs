1fd3216e288d96683cd40c6a4772d89ca89919f6
Pablo Treo <pablotreo@gmail.com>
2023-08-14 11:18:55 -0300

    una arreglo de ultimo moemnto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react(), sitemap()],
+  // site: 'https://campingbellavista.com.ar/',
+  // integrations: [sitemap()]
 });
